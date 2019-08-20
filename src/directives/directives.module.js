6c9ce1a637c69ee8c8857e844287c2d7b7efe5fd
Karla Martín <kmartin@akamai.com>
2019-08-19 22:24:07 -0500

    Declaring & using contact-card directive. Phase 1: Using $scope

--- src/directives/directives.module.js
+++ src/directives/directives.module.js
@@ -1,6 +1,8 @@
 import angular from 'angular';
 import TextBold from './text-bold/text-bold.directive';
+import ContactCard from './contact-card/contact-card.directive'
 
 export default angular.module('custom.directives', [])
     .directive('textBold', TextBold)
+    .directive('contactCard', ContactCard)
     .name;
