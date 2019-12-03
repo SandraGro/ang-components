9cfaec5c359b78139d9aa297ea5a1cb094ce6fa7
Karla Martin <karla.martin@unosquare.com>
2019-12-03 15:50:32 -0600

    Use img-container directive for debugging print styles issue

--- src/screens/demo-directives/directives.controller.js
+++ src/screens/demo-directives/directives.controller.js
@@ -5,6 +5,11 @@ class DirectivesScreenCtrl {
             name: 'Karla Martin',
             position: 'Software Engineer'
         }
+        // this.image = "unosquare_logo.png";
+        this.image = "e1.jpg";
+        // this.image2 = "veelo_logo.png";
+        this.image2 = "e1.jpg";
+        this.image3 = "e1.jpg";
     }
 
 }
